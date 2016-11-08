d1e373ec170dd7e81d11f5300f6ef0d341c400d0
root <root@localhost.localdomain>
2016-11-08 21:17:35 +0100

    Server can now manage multiple users

--- Server/server.py
+++ Server/server.py
@@ -3,21 +3,53 @@
 #################################################################################
 
 import socket
-import configparser
+import select
 
-####### READ THE CONFIG FILE #######
-config = configparser.ConfigParser()
-config.read('./server.conf')
-port = int(config['server']['Port'])
-queue = int(config['server']['Queue size'])
+####### CONFIG #######
+host = ''
+port = 333
+queue = 5
+packetSize = 1024
+timeout = 0.05
 
-####### INIT THE SERVER #######
+####### INITIALIZATION #######
 pynet = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-pynet.bind(('', port))
-print("Server started on port {}".format(port))
-
+pynet.bind((host, port))
 pynet.listen(queue)
 
-client, client_infos = pynet.accept()
-print("New connection = {0}:{1}".format(client_infos[0],client_infos[1]))
-client.send(b"Connection accepted")
+print("Server is up on port : {}".format(port))
+
+serverUp = True
+connectedClients = []
+
+####### MAIN LOOP #######
+
+while serverUp:
+    clients, wlist, xlist = select.select([pynet], [], [], timeout)
+
+    for client in clients:
+        clientSocket, clientInfo = client.accept()
+        connectedClients.append(clientSocket)
+
+    clientsToRead = []
+    try:
+        clientsToRead, wlist, xlist = select.select(connectedClients,[], [], timeout)
+    except select.error:
+        pass
+    else:
+        for client in clientsToRead:
+            message = client.recv(packetSize)
+            message = message.decode()
+            print("Message = {}".format(message))
+            print("Debug = {}".format(message))
+            client.send(b"Successfully received\n")
+            if message[] == "END":
+                serverUp = False
+
+####### SERVER SHUTDOWN #######
+
+print("Server is shutting down")
+for client in connectedClients:
+    client.close()
+
+pynet.close()
